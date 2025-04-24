c302ecea780ab7178d3f7513e529770757b5b909
andrewchang03 <yc16036@gmail.com>
2025-04-24 16:46:39 -0400

    faiss experiments tent. complete

---
+++ src/exp_faiss.py
@@ -0,0 +1,211 @@
+import faiss
+import numpy as np
+from scipy import sparse
+from collections import defaultdict
+from sklearn.random_projection import SparseRandomProjection
+import matplotlib.pyplot as plt
+import os
+
+import util
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def create_faiss_flat(data: np.ndarray):
+    """
+    Creates exact cosine similarity index on Faiss.
+    Basically just storing the data.
+    """
+    index = faiss.IndexFlatIP(data.shape[1])
+    index.add(data)
+    return index
+
+def create_faiss_lsh(data: np.ndarray, nbits: int):
+    """
+    Creates Faiss LSH index with nbits hash.
+    """
+    index = faiss.IndexLSH(data.shape[1], nbits)
+    index.add(data)
+    return index
+
+def random_faiss_experiment(n, d, density, nbits, k):
+    """
+    Generate (n, d) random data of specified density.
+    Compute top-k with Faiss flat and lsh index
+    Return precision, recall, and f-score of LSH vs flat index (ground truth)
+    """
+    data = util.create_random(n, d, density, seed=None)
+
+    flat = create_faiss_flat(data)
+    lsh = create_faiss_lsh(data, nbits)
+
+    q = util.create_random(1, d, density, seed=None)
+
+    _, idx_flat = flat.search(q, k=k)
+    _, idx_lsh = lsh.search(q, k=k)
+
+    idx_flat = set(idx_flat.flatten())
+    idx_lsh = set(idx_lsh.flatten())
+
+    precision, recall, f = util.stats(idx_flat, idx_lsh)
+    return precision, recall, f
+
+def faiss_nbits(n, d, density, nbits, k):
+    """
+    Test Faiss over varying number of hash bits
+    """
+    nbits_x = [8, 16, 32, 64, 256, 512, 1024, 2048, 4096]
+    nbits_y = []
+    for x in nbits_x:
+        p, _, _ = util.avg_over_five(
+            random_faiss_experiment, n, d, density, x, k)
+        nbits_y.append(p)
+    
+    # Plot
+    plt.plot(nbits_x, nbits_y)
+    plt.xlabel('Number of Hash Bits')
+    plt.ylabel('Precision')
+    plt.title('Faiss LSH Precision vs Number of Hash Bits')
+    path = util.absolute_path(BASE_DIR, '..', 'faiss_results', 'faiss_nbits.png')
+    plt.savefig(path)
+
+def faiss_sparsity(n, d, density, nbits, k):
+    """
+    Test Faiss over varying sparsity (1 - density)
+    """
+    sparsity_x = [0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0]
+    sparsity_y = []
+    for x in sparsity_x:
+        p, _, _ = util.avg_over_five(
+            random_faiss_experiment, n, d, 1 - x, nbits, k)
+        sparsity_y.append(p)
+    
+    # Plot
+    plt.plot(sparsity_x, sparsity_y)
+    plt.xlabel('Data Sparsity')
+    plt.ylabel('Precision')
+    plt.title('Faiss LSH Precision vs Data Sparsity')
+    path = util.absolute_path(BASE_DIR, '..', 'faiss_results', 'faiss_sparsity.png')
+    plt.savefig(path)
+
+def faiss_dimensions(n, d, density, nbits, k):
+    """
+    Test Faiss over varying data dimensions d
+    """
+    dim_x = [10, 100, 500, 1000, 5000, 10000]
+    dim_y = []
+    for x in dim_x:
+        p, _, _ = util.avg_over_five(
+            random_faiss_experiment, n, x, density, nbits, k)
+        dim_y.append(p)
+    
+    # Plot
+    plt.plot(dim_x, dim_y)
+    plt.xlabel('Data Dimensionality (log)')
+    plt.ylabel('Precision')
+    plt.xscale('log')
+    plt.title('Faiss LSH Precision vs Data Dimensionality')
+    path = util.absolute_path(
+        BASE_DIR, '..', 'faiss_results', 'faiss_dimensionality.png')
+    plt.savefig(path)
+
+def save_flat_topk(name, k=50):
+    """
+    Save exact top-[k] index of dataset [name]
+    """
+    data = util.load_npz_dense(
+        util.absolute_path(BASE_DIR, '..', 'data', name + '.npz'))
+    flat = create_faiss_flat(data)
+    _, idx_flat = flat.search(data, k=k)
+
+    path = util.absolute_path(BASE_DIR, '..', 'faiss_results', name + '_top50.npy')
+    with open(path, 'wb') as f:
+        np.save(f, idx_flat)
+
+def faiss_dataset(name, nbits, k=50, n_components=500, reduce=False, seed=42):
+    """
+    Evaluate Faiss LSH vs Flat on real datasets.
+    [reduce] if you want to reduce dimensionality of data
+    """
+    data = util.load_npz_dense(
+        util.absolute_path(BASE_DIR, '..', 'data', name + '.npz'))
+    idx_flat = np.load(
+        util.absolute_path(BASE_DIR, '..', 'faiss_results', name + '_top50.npy'))
+    # print('read done')
+
+    if reduce:
+        srp = SparseRandomProjection(n_components=n_components, random_state=seed)
+        data = srp.fit_transform(data)
+        # print(data.shape)
+
+    timer = util.Timer()
+    lsh = create_faiss_lsh(data, nbits)
+    index_elapsed = timer.measure()
+
+    timer.start()
+    _, idx_lsh = lsh.search(data, k=k)
+    search_elapsed = timer.measure()
+
+    precisions = []
+
+    for i in range(len(idx_flat)):
+        idx_flat_row = set(idx_flat[i].flatten())
+        idx_lsh_row = set(idx_lsh[i].flatten())
+        p, _, _ = util.stats(idx_flat_row, idx_lsh_row)
+        precisions.append(p)
+
+    return round(sum(precisions) / len(precisions), 5), index_elapsed, search_elapsed
+
+def faiss_dataset_nbits(name, k=50):
+    nbits_x = [32, 64, 128, 256, 512, 1024, 2048, 4096]
+    nbits_y = []
+    nbits_ie = []  # indexing runtime
+    nbits_se = []  # search runtime
+    nbits_reduced_y = []
+    nbits_reduced_ie = []  # indexing runtime
+    nbits_reduced_se = []  # search runtime
+
+    for nbits in nbits_x:
+        print(nbits)
+        p, ie, se = faiss_dataset(name, nbits)
+        p_reduced, ie_reduced, se_reduced = faiss_dataset(name, nbits, reduce=True)
+        nbits_y.append(p)
+        nbits_reduced_y.append(p_reduced)
+        nbits_ie.append(ie)
+        nbits_reduced_ie.append(ie_reduced)
+        nbits_se.append(se)
+        nbits_reduced_se.append(se_reduced)
+
+    plt.plot(nbits_x, nbits_y, label='Original')
+    plt.plot(nbits_x, nbits_reduced_y, label='Projected')
+    plt.xlabel('Number of LSH Hash Bits')
+    plt.ylabel('Precision')
+    plt.title('Precision vs Number of LSH Hash Bits on ' + name)
+    plt.legend()
+    plt.savefig(util.absolute_path(
+        BASE_DIR, '..', 'faiss_results', 'faiss_' + name + '.png'))
+
+    print(nbits_ie)
+    print(nbits_reduced_ie)
+    print(nbits_se)
+    print(nbits_reduced_se)
+
+if __name__ == "__main__":
+    n = 1000
+    d = 100
+    density = 0.7
+    nbits = 256
+    k = 50
+
+    # Faiss random experiments
+    # faiss_nbits(n, d, density, nbits, k)
+    # faiss_sparsity(n, d, density, nbits, k)
+    # faiss_dimensions(n, d, density, nbits, k)
+    
+    # save_flat_topk('movie', k=50)
+    # save_flat_topk('lastfm', k=50)
+
+    # avg_precision_movie, ie, se = faiss_dataset('movie', 1024)
+    # avg_precision_movie, ie, se = faiss_dataset('movie', 1024, reduce=True)
+    
+    # faiss_dataset_nbits('lastfm')
+    # faiss_dataset_nbits('movie')
---
+++ src/util.py
@@ -0,0 +1,90 @@
+from datetime import datetime
+from scipy import sparse
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+import os
+
+class Timer():
+    """
+    Timer class. start() to reset timer, measure() to stop and get time elapsed
+    """
+    def __init__(self):
+        self.current = datetime.now()
+
+    def start(self):
+        self.current = datetime.now()
+
+    def measure(self) -> int:
+        return (datetime.now() - self.current).total_seconds() * 1000
+    
+def create_random(n, d, density, normalize=True, seed=42):
+    """
+    Creates (n, d) data with density=[density] (in range [0, 1])
+    """
+    data = sparse.random(n, d, density=density, format='csr', random_state=seed)
+    data = data.toarray().astype('float32')
+    if normalize:
+        norms = np.linalg.norm(data, axis=1, keepdims=True)
+        norms = np.where(norms == 0, 1, norms)
+        data /= norms
+    return data
+
+def sklearn_flat(q, data, k) -> np.ndarray:
+    """ 
+    Return top-k indices using sklearn cosine similarity
+
+    Args:
+        q: (m, d) ndarray matrix, set of query vectors
+        data: (n, d) ndarray matrix
+        k: int, how many top k elements
+
+    Returns:
+        (m, k) ndarray with each row containing top k indices
+    """
+    cosines = cosine_similarity(q, data)
+    top_k = np.argsort(cosines)
+    top_k = top_k[:,::-1]
+    top_k = top_k[:,:k]
+    return top_k
+
+def stats(exact: set, estimate: set):
+    """
+    Returns precision, recall, f-score on [exact] ground truth set and
+    predicted [estimate]
+    """
+    tp = len(exact & estimate)
+    precision = tp / len(estimate)
+    recall = tp / len(exact)
+    f = (2 * precision * recall) / (precision + recall)
+    return precision, recall, f
+
+def avg_over_five(func, *args, **kwargs):
+    """
+    Run 5 trials of [func] and return average precision, recall, and f-score
+    """
+    total = 0
+    precision_total = recall_total = f_total = 0
+    for _ in range(5):
+        p, r, f = func(*args, **kwargs)
+        precision_total += p
+        recall_total += r
+        f_total += f
+    precision_avg = round(precision_total / 5, 5)
+    recall_avg = round(recall_total / 5, 5)
+    f_avg = round(f_total / 5, 5)
+    return precision_avg, recall_avg, f_avg
+
+def absolute_path(base, *paths):
+    return os.path.join(base, *paths)
+
+def load_npz_dense(path, normalize=True):
+    """
+    Loads npz file (csr matrix) from [path] 
+    and returns densified np.ndarray of it
+    """
+    data = sparse.load_npz(path).toarray().astype('float32')
+    if normalize:
+        norms = np.linalg.norm(data, axis=1, keepdims=True)
+        norms = np.where(norms == 0, 1, norms)
+        data /= norms
+    return data
